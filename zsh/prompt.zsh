1dcf190396ff1872c716c457cdf79815c93cfac5
Rodrigo Haenggi <rodrigo@codegestalt.com>
2012-06-26 12:12:55 +0200

    update git path

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,7 +3,7 @@ autoload colors && colors
 # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
 git_branch() {
-  echo $(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+  echo $(/usr/bin/env git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
 }
 
 git_dirty() {
