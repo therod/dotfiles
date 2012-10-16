4e88a3ccc6f7caeeb221cf09831a2c941e3b5237
Rodrigo Haenggi <rodrigo@codegestalt.com>
2012-10-16 23:49:51 +0200

    back to homebrew

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
- export PATH="./bin:$HOME/.rbenv/bin:$HOME/.rbenv/shims:/opt/local/bin:/opt/local/sbin:$HOME/tomcat6/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
- export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:/opt/local/share/man:$MANPATH"
+ export PATH="./bin:$HOME/.rbenv/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+ export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
