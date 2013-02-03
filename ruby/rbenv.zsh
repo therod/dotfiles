3a25b30b3ddb188fd63543f297ee88918f1e2a0e
Rodrigo Haenggi <rodrigo@codegestalt.com>
2013-02-03 18:33:13 +0100

    make sure vim has same ruby version as terminal

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,2 +1,13 @@
 export PATH="$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
+
+# Add rbenv init to your shell to enable shims and autocompletion
+if [[ -d ~/.rbenv ]]; then
+  eval "$(rbenv init -)"
+fi
+
+# @see http://vim.1045645.n5.nabble.com/MacVim-and-PATH-tt3388705.html#a3392363
+# Ensure vim has same shell as Terminal
+if [[ -a /etc/zshenv ]]; then
+  sudo mv /etc/zshenv /etc/zprofile
+fi
