48ec92b409c54cf1f8614a7ffbebdf3e674d03f2
Rodrigo Haenggi <rodrigo@codegestalt.com>
2015-04-13 14:26:40 +0200

    hammerspoon initial config

---
+++ hammerspoon/hammerspoon.symlink/init.lua
@@ -0,0 +1,21 @@
+-- Main Configuration
+hs.window.animationDuration = 0         -- Do not animate window movements
+hs.mainModifierKeys = {"ctrl", "cmd"}   -- Variable that holds the main modifier keys !!IMPORTANT!!
+
+-- Load custom Modules
+require('modules/windows')
+
+-- Reload function
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
