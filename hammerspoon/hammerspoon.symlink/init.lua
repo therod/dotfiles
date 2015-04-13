d1b9161fb8a0401a39604a45acfdaefb74efa5a5
Rodrigo Haenggi <rodrigo@codegestalt.com>
2015-04-13 14:55:21 +0200

    add windowHints and other stuff

--- hammerspoon/hammerspoon.symlink/init.lua
+++ hammerspoon/hammerspoon.symlink/init.lua
@@ -2,9 +2,30 @@
 hs.window.animationDuration = 0         -- Do not animate window movements
 hs.mainModifierKeys = {"ctrl", "cmd"}   -- Variable that holds the main modifier keys !!IMPORTANT!!
 
+-- Load Extensions
+local application = require "hs.application"
+local window = require "hs.window"
+local hotkey = require "hs.hotkey"
+local keycodes = require "hs.keycodes"
+local fnutils = require "hs.fnutils"
+local alert = require "hs.alert"
+local screen = require "hs.screen"
+local grid = require "hs.grid"
+local hints = require "hs.hints"
+local appfinder = require "hs.appfinder"
+
 -- Load custom Modules
 require('modules/windows')
 require('modules/caffeine/caffeine')
+
+-- Hotkey Hints
+hotkey.bind(hs.mainModifierKeys,"s",hints.windowHints)
+
+-- URL Example
+-- This alert can be triggered by opening following url: `open -g hammerspoon://someAlert`
+hs.urlevent.bind("someAlert", function(eventName, params)
+    hs.alert.show("Received someAlert")
+end)
 
 -- Reload function
 function reloadConfig(files)
