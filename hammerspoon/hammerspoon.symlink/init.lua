e448ebca0e365bf0d28f71cb75df45593e3ad0bd
Rodrigo Haenggi <rodrigo@codegestalt.com>
2015-07-30 12:37:26 +0200

    disable hammerspoon stuff

--- hammerspoon/hammerspoon.symlink/init.lua
+++ hammerspoon/hammerspoon.symlink/init.lua
@@ -3,24 +3,18 @@ hs.window.animationDuration = 0         -- Do not animate window movements
 hs.mainModifierKeys = {"alt", "cmd"}   -- Variable that holds the main modifier keys !!IMPORTANT!!
 
 -- Load Extensions
-local application = require "hs.application"
 local window = require "hs.window"
 local hotkey = require "hs.hotkey"
-local keycodes = require "hs.keycodes"
 local fnutils = require "hs.fnutils"
 local alert = require "hs.alert"
 local screen = require "hs.screen"
 local grid = require "hs.grid"
 local hints = require "hs.hints"
-local appfinder = require "hs.appfinder"
 
 -- Load custom Modules
 require('modules/windows')
 require('modules/shortcuts')
 -- require('modules/caffeine/caffeine')
-
--- Hotkey Hints
-hotkey.bind(hs.mainModifierKeys,"s",hints.windowHints)
 
 -- URL Example
 -- This alert can be triggered by opening following url: `open -g hammerspoon://someAlert`
