bf301b75d921e08f498b7b4cd94dfc805b446eaf
Rodrigo Haenggi <rodrigo@codegestalt.com>
2016-10-01 11:07:16 +0200

    update tmux aliases

--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -1,6 +1,4 @@
-# set colorscheme for tmux
 alias tlist="tmux list-sessions"
-# alias tmux="TERM=screen-256color-bce tmux -u"
 alias tmux="env TERM=xterm-256color tmux"
 alias tkill="tmux kill-session -t $*"
 alias tnew="tmux new-session -s $*"
