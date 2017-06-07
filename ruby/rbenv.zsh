da92377c90a3ea8c7b045d1bd738ca1a468ced35
Rodrigo Haenggi <rodrigo@codegestalt.com>
2017-06-07 09:50:20 +0200

    fix rbenv

--- ruby/rbenv.zsh
+++
@@ -1,7 +0,0 @@
-export PATH="$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
-# Ensure vim has same shell as Terminal
-if [[ -a /etc/zshenv ]]; then
-  sudo mv /etc/zshenv /etc/zprofile
-fi
